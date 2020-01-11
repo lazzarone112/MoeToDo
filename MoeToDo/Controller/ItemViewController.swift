3773735b14285fed407cbf7baa82aa66adbbc542
M <mohamed.aboghali@gmail.com>
2020-01-11 19:57:26 +0200

    ItemArrayRealmCompleted

--- MoeToDo/Controller/ItemViewController.swift
+++ MoeToDo/Controller/ItemViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ItemViewController: UITableViewController {
     
+    var itemArray :Results<Items>?
+    
+   let realm = try! Realm()
     
     var selectedCategory:Categories? {
+        
         didSet{
             
+            itemArray = selectedCategory?.items.sorted(byKeyPath: "name", ascending: true)
         }
     }
 
@@ -22,7 +28,73 @@ class ItemViewController: UITableViewController {
 
       
     }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray?.count ?? 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray?[indexPath.row].name ?? "No item added.."
+        
+        if itemArray?[indexPath.row].done == true {
+            cell.accessoryType = .checkmark
+        }else{
+            cell.accessoryType = .none
+        }
+        
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = itemArray?[indexPath.row] {
+            do {
+                  try realm.write {
+                    cell.done = !cell.done
+                   }
+               }catch{
+                   
+                   print(error)
+               }
+        }
+        tableView.reloadData()
+    }
+    
+    
+    
+    
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var intermediateItemText = UITextField()
+        
+        let alert = UIAlertController(title: "Add Item", message: nil, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newItem = Items()
+            newItem.name = intermediateItemText.text!
+            do {
+               try self.realm.write {
+                self.selectedCategory?.items.append(newItem)
+                }
+            }catch{
+                print(error)
+            }
+            self.tableView.reloadData()
+        
+        }
+        alert.addAction(action)
+        
+        alert.addTextField { (textField) in
+            
+            intermediateItemText =  textField
+        }
+        
+        present(alert, animated: true, completion: nil)
+        
     }
     
 }
